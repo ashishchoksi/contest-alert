4daf20fd7682b8f3d1dde0cf5bc2babb7ac88737
ashish choksi <ashishchoksi84@gmail.com>
2021-04-11 15:05:47 +0530

    fixed header

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -18,6 +18,7 @@ function AboutUs() {
                 </div>
             </div>
         </div>
+
     );
 }
 
--- src/components/header/NavBar/NavBar.js
+++ src/components/header/NavBar/NavBar.js
@@ -4,15 +4,18 @@ import logo from './logo.png';
 
 function NavBar() {
     return (
+
         <div className="container-fluid">
-            <div className="row top-nav">
-                <ul className="col-md-8 left-side col-sm-12">
-                    <li> Contest-Alert </li>
-                    <li>Upcomming Contest</li>
-                </ul>
-                <ul className="col-md-4 text-right left-side col-sm-12">
-                    <li>Login</li>
-                </ul>
+            <div className="fixed">
+                <div className="row top-nav">
+                    <ul className="col-md-8 left-side col-sm-12">
+                        <li> Contest-Alert </li>
+                        <li>Upcomming Contest</li>
+                    </ul>
+                    <ul className="col-md-4 text-right left-side col-sm-12">
+                        <li>Login</li>
+                    </ul>
+                </div>
             </div>
         </div>
     );
